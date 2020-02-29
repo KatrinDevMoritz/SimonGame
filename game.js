1c04d702adb366ec1ca0db7c7b4d33a6463d076f
KatrinDevMoritz <58624173+KatrinDevMoritz@users.noreply.github.com>
2020-02-29 09:23:39 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,22 +1,10 @@
-
-var buttonColours = ["red", "blue", "green", "yellow"];
-
+var buttonColours = ["red", "blue", "green","yellow"];
 var gamePattern = [];
-
-//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
 var userClickedPattern = [];
 
-//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
 $(".btn").click(function() {
-
-  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
-  var userChosenColour = $(this).attr("id");
-
-  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
-  userClickedPattern.push(userChosenColour);
-
-  //console.log(userClickedPattern);
-
+var userChosenColour = $(this).attr("id");
+userClickedPattern.push(userChosenColour);
 });
 
 function nextSequence() {
@@ -27,6 +15,6 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  var audio = new Audio('sounds/' + randomChosenColour + 'mp3')
   audio.play();
 }
